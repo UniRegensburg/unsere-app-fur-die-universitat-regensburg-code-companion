d3d1c38f7ef41b0eec538f2f5091b8905b205e9f
Mathias Götz <magoe1997@gmail.com>
2020-12-14 14:59:12 +0100

    [FEATURE] Added java wireframe for webrtc

---
+++ Java/src/main/java/MainActivity.java
@@ -0,0 +1,13 @@
+
+import dev.onvoid.webrtc.PeerConnectionFactory;
+import dev.onvoid.webrtc.RTCDataChannel;
+import dev.onvoid.webrtc.RTCPeerConnection;
+
+import java.net.Socket;
+
+public class MainActivity {
+    private RTCPeerConnection peerConnection;
+    private PeerConnectionFactory factory;
+    private RTCDataChannel dc;
+
+}
