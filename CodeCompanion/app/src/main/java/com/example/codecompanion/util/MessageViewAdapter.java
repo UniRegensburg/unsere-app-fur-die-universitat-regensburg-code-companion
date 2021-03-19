fece38599534f79043a132027fd0ec327435853c
Fabian Schiller <schiller.fabian25@gmail.com>
2021-03-19 19:38:22 +0100

    [feature] Added onclicklistener and new fragment

--- CodeCompanion/app/src/main/java/com/example/codecompanion/util/MessageViewAdapter.java
+++ CodeCompanion/app/src/main/java/com/example/codecompanion/util/MessageViewAdapter.java
@@ -1,13 +1,17 @@
 package com.example.codecompanion.util;
-
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 import com.example.codecompanion.R;
+import com.example.codecompanion.ui.compiler.ExpandedMessageFragment;
+
 import java.util.List;
 import java.util.Map;
 
@@ -29,10 +33,20 @@ public class MessageViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHo
      */
     class ErrorViewHolder extends RecyclerView.ViewHolder {
         private final TextView textView;
+        private final ConstraintLayout layout;
 
         public ErrorViewHolder(View view) {
             super(view);
             textView = (TextView) view.findViewById(R.id.error_rv_template);
+            layout = (ConstraintLayout) view.findViewById(R.id.error);
+            layout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    AppCompatActivity activity = (AppCompatActivity) view.getContext();
+                    Fragment fragment = new ExpandedMessageFragment();
+                    activity.getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, fragment).addToBackStack(null).commit();
+                }
+            });
         }
     }
 
@@ -41,10 +55,20 @@ public class MessageViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHo
      */
     class WarningViewHolder extends RecyclerView.ViewHolder {
         private final TextView textView;
+        private final ConstraintLayout layout;
 
         public WarningViewHolder(View view) {
             super(view);
             textView = (TextView) view.findViewById(R.id.warning_rv_template);
+            layout = (ConstraintLayout) view.findViewById(R.id.warning);
+            layout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    AppCompatActivity activity = (AppCompatActivity) view.getContext();
+                    Fragment fragment = new ExpandedMessageFragment();
+                    activity.getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, fragment).addToBackStack(null).commit();
+                }
+            });
         }
     }
 
