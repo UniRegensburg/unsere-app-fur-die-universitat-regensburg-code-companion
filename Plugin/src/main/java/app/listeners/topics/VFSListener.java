d0b7ebd28d0045e368c711fbc3460ac4c41e255c
Mathias Götz <magoe1997@gmail.com>
2021-03-25 18:30:43 +0100

    Resend Tasks on json change

---
+++ Plugin/src/main/java/app/listeners/topics/VFSListener.java
@@ -0,0 +1,32 @@
+package app.listeners.topics;
+
+import app.services.application.ApplicationService;
+import com.intellij.openapi.components.ServiceManager;
+import com.intellij.openapi.vfs.newvfs.BulkFileListener;
+import com.intellij.openapi.vfs.newvfs.events.VFileEvent;
+import dev.onvoid.webrtc.RTCPeerConnectionState;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class VFSListener implements BulkFileListener {
+    private final ApplicationService manager;
+
+    public VFSListener() {
+        manager = ServiceManager.getService(ApplicationService.class);
+    }
+
+    @Override
+    public void after(@NotNull List<? extends VFileEvent> events) {
+        if(manager.getWebRTC().getConnectionState() == RTCPeerConnectionState.CONNECTED) {
+            System.out.println(events.toString()+manager.getTaskHandler().isSent());
+            if (!manager.getTaskHandler().isSent()) {
+                manager.getTaskHandler().sendTaskInfo();
+            }
+            if (events.toString().contains("task.json")) {
+                System.out.println("JSON Changed");
+                manager.getTaskHandler().sendTaskInfo();
+            }
+        }
+    }
+}
