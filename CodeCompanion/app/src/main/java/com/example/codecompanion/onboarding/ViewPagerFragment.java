75667d41a905821ae18693d74e73aa27fe57e126
Fabian Schiller <schiller.fabian25@gmail.com>
2021-04-07 22:26:52 +0200

    [feature] Added tutorial fragments and adjusted nav component
    wip: functionality of skipping / next buttons

---
+++ CodeCompanion/app/src/main/java/com/example/codecompanion/onboarding/ViewPagerFragment.java
@@ -0,0 +1,41 @@
+package com.example.codecompanion.onboarding;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.codecompanion.R;
+import com.example.codecompanion.onboarding.screens.FirstScreen;
+import com.example.codecompanion.onboarding.screens.FourthScreen;
+import com.example.codecompanion.onboarding.screens.SecondScreen;
+import com.example.codecompanion.onboarding.screens.ThirdScreen;
+
+import java.util.ArrayList;
+
+public class ViewPagerFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_view_pager, container, false);
+        ArrayList<Fragment> fragmentList = new ArrayList<>();
+        fragmentList.add(new FirstScreen());
+        fragmentList.add(new SecondScreen());
+        fragmentList.add(new ThirdScreen());
+        fragmentList.add(new FourthScreen());
+
+        ViewPagerAdapter adapter = new ViewPagerAdapter(requireActivity().getSupportFragmentManager(), getLifecycle(), fragmentList);
+        ViewPager2 viewPager = view.findViewById(R.id.viewpager);
+        viewPager.setAdapter(adapter);
+
+
+        return view;
+    }
+}
