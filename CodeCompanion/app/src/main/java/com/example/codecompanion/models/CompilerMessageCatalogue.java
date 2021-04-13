809162707172522d06b15e538a3ae55000f5d330
Seewald Maximilian <Maximilian-Seewald@gmx.net>
2021-04-13 11:51:08 +0200

    [feature] Added some new Error Messages

--- CodeCompanion/app/src/main/java/com/example/codecompanion/models/CompilerMessageCatalogue.java
+++ CodeCompanion/app/src/main/java/com/example/codecompanion/models/CompilerMessageCatalogue.java
@@ -54,8 +54,68 @@ public class CompilerMessageCatalogue {
 			return "Your class is not used anywhere. Check where you wanted to use it!";
 		}
 
+		if(description.matches("Parameter (.*) is never used")){
+
+			Pattern p = Pattern.compile("Parameter (.*) is never used");
+			Matcher m = p.matcher(description);
+			if (m.find()) {
+				String symbol = m.group(1);
+				return "Your " + symbol + " is never used anywhere. Check whether you need it.";
+			}
+			return "Your parameter is never used anywhere. Check whether you need it.";
+		}
+
+		if(description.matches("Cannot return a value from a method with void result type")){
+			return "You are trying to return something from a void method. Change the method type to none void if you need to return something.";
+		}
+
+		if(description.matches("Condition is always true")){
+			return "The condition is always true.There is no need for checking something if its always true.";
+		}
+
+		if(description.matches("(.*) statement has empty body")){
+			Pattern p = Pattern.compile("(.*) statement has empty body");
+			Matcher m = p.matcher(description);
+			if (m.find()) {
+				String symbol = m.group(1);
+				return "Your " + symbol + " statement is not doing anything.Check if you need your " + symbol + " statement.";
+			}
+			return "Your statement is not doing anything.Check if you need your statement.";
+		}
+
+		if(description.matches("Variable (.*) might not have been initialized")){
+			Pattern p = Pattern.compile("Variable (.*) might not have been initialized");
+			Matcher m = p.matcher(description);
+			if (m.find()) {
+				String symbol = m.group(1);
+				return "Your variable" + symbol + " is never initialized.You should check " + symbol + ".The variable is missing a set value.";
+			}
+			return "Your variable is never initialized.The variable is missing a set value.";
+
+		}
+
+		if(description.matches("Variable (.*) is never assigend")){
+			Pattern p = Pattern.compile("Variable (.*) is never assigend");
+			Matcher m = p.matcher(description);
+			if (m.find()) {
+				String symbol = m.group(1);
+				return "Your variable" + symbol + " is missing a set value.";
+			}
+			return "Your variable is missing a set value.";
+
+		}
+
+		if(description.matches("(.*) expected")){
+			Pattern p = Pattern.compile("(.*) expected");
+			Matcher m = p.matcher(description);
+			if (m.find()) {
+				String symbol = m.group(1);
+				return "You are missing a " + symbol;
+			}
+			return "You are missing a ';' or '('";
+		}
 		// default
-		return "placeholder-short";
+		return "Seems like there is no detailed description for your error. Maybe you can check the web.";
 	}
 
 	/**
--- TemplateProject/src/main.java
+++ TemplateProject/src/main.java
@@ -1,2 +1,14 @@
-main class[
-        ]
+class main {
+
+    public void test(String test){
+        String test2;
+        System.out.println("test");
+        if(true){
+
+        }
+        System.out.println(test2);
+        System.out.println(;
+        System.out.println()
+        return test;
+    }
+}
